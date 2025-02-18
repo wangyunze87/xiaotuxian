c65960fbc0216c3c2182014da28bf2cb6d6bf813
changxin <2140301189@qq.com>
2025-02-18 20:06:08 +0800

    home-面板组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,7 @@ import HomeBanner from "@/views/Home/components/HomeBanner.vue";
 import HomeNew from "@/views/Home/components/HomeNew.vue";
 import HomeHot from "@/views/Home/components/HomeHot.vue";
 import HomeProduct from "@/views/Home/components/HomePeoduct.vue";
+import HomePanel from "@/views/Home/components/HomePanel.vue";
 </script>
 
 <template>
@@ -14,4 +15,12 @@ import HomeProduct from "@/views/Home/components/HomePeoduct.vue";
   <HomeNew />
   <HomeHot />
   <HomeProduct />
+
+  <HomePanel title="新鲜好物" sub-title="新鲜好物">
+    <div>我是新鲜好物的插槽</div>
+  </HomePanel>
+
+  <HomePanel title="人气推荐" sub-title="新鲜好物lll">
+    <div>我是人气推荐的插槽</div>
+  </HomePanel>
 </template>
