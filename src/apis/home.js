fc632da16226dac4a3705843130a6b872eb6bad3
changxin <2140301189@qq.com>
2025-02-19 15:10:39 +0800

    home-图片懒加载

--- src/apis/home.js
+++ src/apis/home.js
@@ -17,3 +17,15 @@ export const findNewAPI = () => {
     url:'/home/new'
   })
 }
+
+/**
+ * @description: 获取人气推荐
+ * @param {*}
+ * @return {*}
+ */
+export const getHotAPI = () => {
+ // return  httpInstance('home/hot', 'get', {})
+ return  httpInstance( {
+  url:'home/hot'
+ })
+}
--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import { getCategory } from './apis/testAPI'
+
+import { useIntersectionObserver } from '@vueuse/core'
 //测试接口函数
 getCategory().then(res=>{
   console.log(res)
@@ -16,3 +18,26 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+//定义全局指令
+
+app.directive('img-lazy',{
+   // el: 指令绑定的那个元素 img
+        // binding: binding.value  指令等于号后面绑定的表达式的值  图片url
+  mounted(el,binding){
+console.log(el,binding.value)
+
+
+useIntersectionObserver(
+  el,
+  ([{ isIntersecting }]) => {
+    console.log(isIntersecting)
+    if (isIntersecting) {
+     // 进入视口区域
+      el.src = binding.value
+      stop()
+    }
+  },
+)
+}
+})
