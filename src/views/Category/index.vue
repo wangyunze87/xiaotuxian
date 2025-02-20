41adde65be3cfc950948b49676825131a78bc9c0
changxin <2140301189@qq.com>
2025-02-20 16:09:08 +0800

    一级分类-解决路由缓存问题

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -3,6 +3,7 @@ import { getCategoryAPI } from "@/apis/category";
 import { ref, watchEffect, onMounted } from "vue";
 import { useRoute } from "vue-router";
 import { getBannerAPI } from "@/apis/home";
+import GoodsItem from "@/views/Home/components/GoodsItem.vue";
 //获取数据
 const categoryData = ref({});
 const route = useRoute();
@@ -42,6 +43,26 @@ onMounted(() => getBanner());
             <img :src="item.imgUrl" alt="" />
           </el-carousel-item>
         </el-carousel>
+      </div>
+
+      <div class="sub-list">
+        <h3>全部分类</h3>
+        <ul>
+          <li v-for="i in categoryData.children" :key="i.id">
+            <RouterLink to="/">
+              <img :src="i.picture" />
+              <p>{{ i.name }}</p>
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+      <div class="ref-goods" v-for="item in categoryData.children" :key="item.id">
+        <div class="head">
+          <h3>- {{ item.name }}-</h3>
+        </div>
+        <div class="body">
+          <GoodsItem v-for="goods in item.goods" :goods="goods" :key="goods.id" />
+        </div>
       </div>
     </div>
   </div>
